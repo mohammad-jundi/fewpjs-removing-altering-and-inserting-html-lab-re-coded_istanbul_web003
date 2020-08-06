e8c40c08c18de50e798e4cc2be5076482e75a5e7
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-08-06 03:39:13 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove()
+
+let newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id= "victory";
+newHeader.innerHTML = "Mohammad is the champion"
